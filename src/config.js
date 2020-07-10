82a04ca9b954195217f3be0a4492eab39238cdfd
Joseph Montesano <jmmmontesano@gmail.com>
2020-07-10 10:26:40 -0400

    :)

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL:
-    process.env.DB_URL ||
+    process.env.DATABASE_URL ||
     'dbname=d8m0ake3mv4cq0 host=ec2-54-234-44-238.compute-1.amazonaws.com port=5432 user=hinqooqwexsptt password=0ec62e8cf62cd67f86ec7077363602692b9c9986c5c4f92660e2503e8a1191aa sslmode=require',
   JWT_SECRET:
     process.env.JWT_SECRET ||
