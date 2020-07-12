435121b9226c097e881cc7c3c7c915c032faac6f
Joseph Montesano <jmmmontesano@gmail.com>
2020-07-12 14:20:15 -0400

    trying to fix cors

--- src/app.js
+++ src/app.js
@@ -11,6 +11,11 @@ const usersRouter = require('./users/users-router');
 
 const app = express();
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganOption));
