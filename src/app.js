8043d40a7128bbd7a3f29a4308464bc158817ed8
Joseph Montesano <jmmmontesano@gmail.com>
2020-07-14 15:26:42 -0400

    trying to fix cors

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const bodyParser = require('body-parser');
 const usersRouter = require('./users/users-router');
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }))
 // app.use(function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
